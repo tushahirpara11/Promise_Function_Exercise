c8c22f580c25423210c9a04a2cb811a877a26f96
tusharhirpara11 <tusharhirpara98@gmail.com>
2020-01-28 15:05:23 +0530

    multiple promises

--- program.js
+++ program.js
@@ -125,3 +125,21 @@ Promise.resolve(iterate(1))
   .then(iterate)
   .then(iterate)
   .catch(onReject);
+
+//11) Multiple Promises
+function all(promise1, promise2) {
+  let result = [];
+  return new Promise(function (resolve, reject) {
+    return promise1.then(function (response) {
+      result.push(response);
+      return promise2.then(function (response) {
+        result.push(response)
+        resolve(result);
+      });
+    });
+  });
+}
+
+all(getPromise1(), getPromise2()).then(function (response) {
+  console.log(response);
+});
